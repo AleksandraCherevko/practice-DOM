55409d619a9a04e5e11f9c19dd2ef01cf40604b3
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-02-02 22:04:32 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,19 @@
 // скрипт посчитает и выведет в консоль количество категорий в  ul#categories, то есть элементов  li.item.
 
-const categoryList = document.querySelector("#categories");
-console.log(categoryList);
+const refs = {
+  categoryListEl: document.querySelector("#categories"),
+  listItemsEl: document.querySelectorAll(".item"),
+  titleEl: document.querySelectorAll("h2"),
+  categoryEl: document.querySelectorAll("ul"),
+};
 
-const listItems = document.querySelector(".item");
-console.log(listItems);
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
+// елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+
+console.log(`Number of categories: ${refs.listItemsEl.length}`);
+console.log(refs.titleEl);
+console.log(refs.categoryEl);
+
+refs.titleEl.forEach((el) => {
+  console.log(el);
+});
