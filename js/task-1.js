541638db0eb99b239fb4689bc1f5035895af3d2b
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-02-02 18:20:55 +0100

    upd

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+// скрипт посчитает и выведет в консоль количество категорий в  ul#categories, то есть элементов  li.item.
+
+const categoryList = document.querySelector("#categories");
+console.log(categoryList);
+
+const listItems = document.querySelector(".item");
+console.log(listItems);
