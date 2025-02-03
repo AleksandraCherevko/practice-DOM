7ec122a9aacf9bc465685ac87e3e2e4fc5bd0e9f
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-02-03 16:13:18 +0100

    task 1 upd

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,31 @@
 // скрипт посчитает и выведет в консоль количество категорий в  ul#categories, то есть элементов  li.item.
-
-const refs = {
-  categoryListEl: document.querySelector("#categories"),
-  listItemsEl: document.querySelectorAll(".item"),
-  titleEl: document.querySelectorAll("h2"),
-  categoryEl: document.querySelectorAll("ul"),
-};
-
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
 // елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
-console.log(`Number of categories: ${refs.listItemsEl.length}`);
-console.log(refs.titleEl);
-console.log(refs.categoryEl);
+const allCategories = document.querySelectorAll(".item");
 
-refs.titleEl.forEach((el) => {
-  console.log(el);
+console.log(`Number of categories: ${allCategories.length}`);
+
+allCategories.forEach((category) => {
+  categoriesName(category);
 });
+
+function categoriesName(element) {
+  const titleEl = element.querySelector("h2");
+  const listEl = element.querySelector("ul");
+  const categoryCount = element.querySelectorAll("li");
+
+  console.log(`Category: ${titleEl.textContent}`);
+  console.log(`Elements: ${categoryCount.length}`);
+
+  titleEl.classList.add("js-title-task-first");
+  listEl.classList.add("js-list-task-first");
+
+  categoryCount.forEach((item) => {
+    item.classList.add("js-item-task-first");
+  });
+}
+
+// styles
+const bodyColor = document.querySelector("body");
+bodyColor.classList.add("js-body-task-first");
