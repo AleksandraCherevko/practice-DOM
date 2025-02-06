0343de6d563d18ac5f84225a202e17cb5d4d49e5
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-02-06 19:36:49 +0100

    finish task 3
    A script that, while typing text in the input#name-input (input event), inserts its current value into the span#name-output as the name for the greeting. To trim the input value to remove leading and trailing spaces. If the input is empty or contains only spaces, the span should display the string "Anonymous" instead of the name.

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,18 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
+inputEl.classList.add("input-task-three");
+outputEl.classList.add("output-task-three");
+const helloTitle = document.querySelector("h1");
+helloTitle.classList.add("hello-title-task-three");
+
+const inputDiv = document.createElement("div");
+inputDiv.classList.add("input-container-task-three");
+inputDiv.append(inputEl, helloTitle);
+
+document.body.append(inputDiv);
+
 const onInput = (event) => {
-  const inputValue = inputEl.value.trim();
-  console.log(inputValue);
-  return (outputEl.textContent = event.currentTarget.value);
+  const inputValue = event.currentTarget.value.trim();
+  outputEl.textContent = inputValue === "" ? "Anonymous" : inputValue;
 };
 inputEl.addEventListener("input", onInput);
