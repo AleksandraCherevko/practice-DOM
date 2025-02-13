9ccb99beb0977cc4bf819eb8296a4e490d28b8e6
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-02-13 11:28:06 +0100

    finish task 4
    Form submission handling – the handleSubmit function is triggered on the submit event of the form.login-form.
    Preventing page reload – event.preventDefault() ensures that the browser does not reload the page after form submission.
    Field validation – if at least one field (email or password) is empty, an alert message alert('All form fields must be filled in') is displayed.
    Collecting data into an object – if all fields are filled, their values are retrieved using loginForm.elements, trimmed of extra spaces, and stored in an object where the keys correspond to the input names.
    Logging data to the console – the object containing the user's input is logged to the console.
    Clearing the form – after successful submission, form.reset() is called to clear all input fields.

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,53 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+form.classList.add("form-task-four");
 
+const submitBtn = document.querySelector("button");
+submitBtn.classList.add("submitBtn-task-four");
+
+const labels = document.querySelectorAll("label");
+labels.forEach((label) => {
+  label.classList.add("label-task-four");
+
+  const div = document.createElement("div");
+  div.classList.add("label-wrapper-task-four");
+
+  label.parentNode.insertBefore(div, label);
+  div.appendChild(label);
+});
+
+const inputs = document.querySelectorAll("input");
+inputs.forEach((input) => {
+  input.classList.add("input-task-four");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const loginForm = event.target;
+  const login = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
+
+  if (login === "" || password === "") {
+    showCustomAlert("All form fields must be filled in !!!");
+    return;
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
+
+function showCustomAlert(message) {
+  if (document.querySelector(".alert-task-four")) return;
+
+  const alertBox = document.createElement("div");
+  alertBox.classList.add("alert-task-four");
+  alertBox.textContent = message;
+
+  document.body.appendChild(alertBox);
+
+  setTimeout(() => {
+    alertBox.classList.add("hide");
+    setTimeout(() => alertBox.remove(), 500);
+  }, 3000);
+}
